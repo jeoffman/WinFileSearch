7343c8228eea809c54c6f7414293f50a8c7dfe05
Jeoff Hines <github@hines.me>
2019-07-28 13:38:11 -0500

    Skeletal WPF project

---
+++ WinFileSearch/CustomTextBlock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace WinFileSearch
+{
+    public class CustomTextBlock : TextBlock
+    {
+        public InlineCollection InlineCollection
+        {
+            get
+            {
+                return (InlineCollection)GetValue(InlineCollectionProperty);
+            }
+            set
+            {
+                SetValue(InlineCollectionProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty InlineCollectionProperty = DependencyProperty.Register(
+            "InlineCollection",
+            typeof(InlineCollection),
+            typeof(CustomTextBlock),
+                new UIPropertyMetadata((PropertyChangedCallback)((sender, args) =>
+                {
+                    CustomTextBlock textBlock = sender as CustomTextBlock;
+
+                    if (textBlock != null)
+                    {
+                        textBlock.Inlines.Clear();
+
+                        InlineCollection inlines = args.NewValue as InlineCollection;
+
+                        if (inlines != null)
+                            textBlock.Inlines.AddRange(inlines.ToList());
+                    }
+                })));
+    }
+}
---
+++ WinFileSearch/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace WinFileSearch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly MyTraceListener _traceListener = new MyTraceListener();
+        private static TraceSource _traceSource = new TraceSource("WinFileSearch", SourceLevels.All);
+
+
+        public MainData MainData { get; set; } = new MainData();
+
+        public MainWindow()
+        {
+            DataContext = MainData;
+
+            InitializeComponent();
+
+            _traceListener.EventHandler += OnTraceEvent;
+            _traceListener.Filter = new EventTypeFilter(SourceLevels.All);
+            Trace.Listeners.Add(_traceListener);
+            _traceSource.Listeners.Add(_traceListener);
+
+            _traceSource.TraceEvent(TraceEventType.Critical, 123, $"Critical");
+            _traceSource.TraceEvent(TraceEventType.Error, 123, $"Error");
+            _traceSource.TraceEvent(TraceEventType.Warning, 123, $"Warning");
+            _traceSource.TraceEvent(TraceEventType.Information, 123, $"Information");
+            _traceSource.TraceEvent(TraceEventType.Verbose, 123, $"Verbose");
+            Trace.WriteLine($"Test Trace");
+            Trace.WriteLine($"Log Trace");
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+            base.OnClosing(e);
+        }
+
+        private void OnTraceEvent(object sender, TraceEventArgs e)
+        {
+            MainData.Traces.Add(new TraceInfo { TimeStamp = DateTime.Now, TraceEventType = e.TraceEventType, Message = e.Message });
+        }
+
+
+    }
+}
